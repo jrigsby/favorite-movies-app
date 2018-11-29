69402e171f680c0b6d5a33cf369dc5326e1e11c0
John Rigsby <johnr@johns-imac.local>
2018-11-29 12:34:35 -0500

    Initial Commit

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by John Rigsby on 11/29/18.
+//  Copyright © 2018 TCR Development, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
